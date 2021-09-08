c30369506ff770193a2a7da5dcca3be9c566e699
TeacherIVKHK <teacherIvkhk@ivkhk.ee>
2021-09-08 11:28:51 +0300

    Используем if

--- src/game/Game.java
+++ src/game/Game.java
@@ -5,6 +5,9 @@
  */
 package game;
 
+import java.util.Random;
+import java.util.Scanner;
+
 /**
  *
  * @author Melnikov
@@ -15,7 +18,17 @@ public class Game {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
+        Random random = new Random();
+        Scanner scanner = new Scanner(System.in);
+        int number = random.nextInt(9-0+1)+0;
+        System.out.print("Программа запомнила число от 0 до 9, угадай! ");
+        int userNumber = scanner.nextInt();
+        if(number == userNumber){
+            System.out.println("Ура! Ты выиграл!!!");
+        }else{
+            System.out.println(":(, неправильно. Задумано число: "+number);
+        }   
+    
     }
     
 }
